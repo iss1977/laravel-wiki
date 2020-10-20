612fc8686b232d2c3120ecf3ccc971625a984641
Ioan-Sebastian Szücs <ioan.szucs@gmail.com>
2020-10-20 15:28:15 +0200

    migration vorbereitet, nicht ausgefuhrt

---
+++ database/migrations/2020_10_20_124912_create_categories_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('username',50);
+            $table->string('password', 100);
+            $table->string('lastname',50);
+            $table->string('firstname',50);
+            $table->string('address',200);
+            $table->timestamp('added_on', 0);
+
+            
+            
+            
+            
+
+
+
+            // $table->foreignId('category_id')->constrained('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/2020_10_20_125312_create_entries_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('teaser',200);
+            $table->text('description');
+            $table->string('img-path',200);
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+            
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
